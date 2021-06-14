191779a6198c064bf6cfd82c81082408abb0cda9
Julian Alfonso Ponce <ponce.julianalfonso@gmail.com>
2021-06-14 20:02:54 +1000

    Make ResultsTable responsive

--- src/components/ResultsTable.tsx
+++ src/components/ResultsTable.tsx
@@ -1,25 +1,33 @@
 import React from "react";
 import { Result } from "../types";
-import { DataTable } from "react-native-paper";
+import { Button, Card, DataTable, Paragraph, Title } from "react-native-paper";
 import { StyleSheet, FlatList } from "react-native";
 import { theme } from "../core/theme";
+import { isLargeScreen } from "../core/screen";
 
 type Props = {
   results: Result[];
   onPress: (resultId: string) => void;
 };
 
-export const ResultsTable: React.FC<Props> = ({ results, onPress }) => {
-  const renderItem = ({ item }: { item: Result }) => {
-    let status = "";
+const getStatusText = (isError: boolean, isDone: boolean): string => {
+  let status = "";
 
-    if (item.isError) {
-      status = "Error";
-    } else if (item.done) {
-      status = "Done";
-    } else {
-      status = "Fetching";
-    }
+  if (isError) {
+    status = "Error";
+  } else if (isDone) {
+    status = "Done";
+  } else {
+    status = "Fetching";
+  }
+
+  return status;
+}
+
+export const ResultsTableLarge: React.FC<Props> = ({ results, onPress }) => {
+  const styles = stylesLarge;
+
+  const renderItem = ({ item }: { item: Result }) => {
     return (
       <DataTable.Row
         key={item.id}
@@ -29,7 +37,7 @@ export const ResultsTable: React.FC<Props> = ({ results, onPress }) => {
         <DataTable.Cell style={styles.positionColumn}>{item.position}</DataTable.Cell>
         <DataTable.Cell style={styles.titleColumn}>{item.title}</DataTable.Cell>
         <DataTable.Cell style={styles.linkColumn}>{item.link}</DataTable.Cell>
-        <DataTable.Cell style={styles.statusColumn}>{status}</DataTable.Cell>
+        <DataTable.Cell style={styles.statusColumn}>{getStatusText(item.isError, item.done)}</DataTable.Cell>
       </DataTable.Row>
     );
   };
@@ -52,7 +60,36 @@ export const ResultsTable: React.FC<Props> = ({ results, onPress }) => {
   );
 };
 
-const styles = StyleSheet.create({
+const ResultsTableMobile: React.FC<Props> = ({ results, onPress }) => {
+  const styles = stylesMobile;
+
+  return (
+    <>
+      {results.map((r) => (
+        <Card style={styles.cardContainer} key={r.id}>
+          <Card.Content>
+            <Title>#{r.position}. {r.title}</Title>
+            <Paragraph>Link: {r.link}</Paragraph>
+            <Paragraph>Status: {getStatusText(r.isError, r.done)}</Paragraph>
+          </Card.Content>
+          <Card.Actions style={styles.actionsContainer}>
+            <Button onPress={() => onPress(r.id)}>View</Button>
+          </Card.Actions>
+        </Card>
+      ))}
+    </>
+  );
+};
+
+export const ResultsTable: React.FC<Props> = (props) => {
+  if (isLargeScreen()) {
+    return (<ResultsTableLarge {...props} />)
+  } else {
+    return (<ResultsTableMobile {...props} />)
+  }
+}
+
+const stylesLarge = StyleSheet.create({
   container: {
     backgroundColor: theme.colors.surface,
   },
@@ -67,5 +104,14 @@ const styles = StyleSheet.create({
   },
   statusColumn: {
     flex: 1,
+  },
+});
+
+const stylesMobile = StyleSheet.create({
+  cardContainer: {
+    marginBottom: 20,
+  },
+  actionsContainer: {
+    justifyContent: "flex-end"
   },
 });
